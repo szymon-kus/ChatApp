23d0dbdb8d7d27822cc021752a9cb470473eaf25
Szymon Kuś <szymon.kus2001@gmail.com>
2024-05-24 18:51:25 +0200

    ..

--- WebChatAppC#GitHub/obj/Debug/net8.0/WebChatAppC#GitHub.AssemblyInfo.cs
+++ WebChatAppC#GitHub/obj/Debug/net8.0/WebChatAppC#GitHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebChatAppC#GitHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce1b32a92a65007fadbb471b529017e34f6b563")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebChatAppC#GitHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebChatAppC#GitHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
